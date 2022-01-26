3d87233224b9de970a6198682ba749e6654ffd21
Mason Bartlett <Mason.Bartlett@vu.com>
2022-01-26 07:54:14 -0600

    added rate variable

--- payCalculator.py
+++ payCalculator.py
@@ -1,18 +1,21 @@
 def calculatePay():
     
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours: ")
+    rate = input ("Enter Rate: ")
     try:
         hrs = float(hrs)
+        rate = float(rate)
     except:
         hrs = -1
+        rate = -1
     
     if hrs > 0 :
         if hrs <= 40 :
-            pay = hrs * 10
+            pay = hrs * rate
             print(pay)
         if hrs > 40 :
-            pay = (40 * 10) + (((hrs - 40) * 10 ) * 1.5)
+            pay = (40 * rate) + (((hrs - 40) * rate ) * 1.5)
             print(pay)
     else :
         print("Error, please enter numeric input")
